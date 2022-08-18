ed7d105ab778c9e74af745c92c188047fa72edd7
Ho Minh <minhhh12@gmail.com>
2022-08-18 11:05:25 +0700

    Remove comment

--- core/base/resources/views/partials/aside.blade.php
+++ core/base/resources/views/partials/aside.blade.php
@@ -1,11 +1,11 @@
 <aside class="main-sidebar sidebar-light-pink elevation-4 sidebar-no-expand">
-	<a href="https://demo.s-cart.org/sc_admin" class="brand-link navbar-secondary">
+	<a href="#" class="brand-link navbar-secondary">
 		M-Cart <span class="brand-text font-weight-light">Admin</span>
 	</a>
 	<div class="sidebar sidebar-lightblue">
 		<nav class="mt-2">
 			<ul class="nav nav-pills nav-sidebar flex-column nav-legacy" data-widget="treeview" role="menu">
-				<form action="https://demo.s-cart.org/sc_admin/order" method="get" class="form-inline m-1 d-block d-sm-none">
+				<form action="#" method="get" class="form-inline m-1 d-block d-sm-none">
 					<div class="input-group input-group-sm">
 						<input name="keyword" class="form-control form-control-navbar" type="search" placeholder="Search order" aria-label="Search">
 						<div class="input-group-append">
@@ -29,7 +29,7 @@
 					</a>
 					<ul class="nav nav-treeview">
 						<li class="nav-item ">
-							<a href="https://demo.s-cart.org/sc_admin/order" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fas fa-shopping-cart nav-icon"></i>
 								<p>Order</p>
 							</a>
@@ -46,13 +46,13 @@
 					</a>
 					<ul class="nav nav-treeview">
 						<li class="nav-item active">
-							<a href="https://demo.s-cart.org/sc_admin/product" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="far fa-file-image nav-icon"></i>
 								<p>Products</p>
 							</a>
 						</li>
 						<li class="nav-item ">
-							<a href="https://demo.s-cart.org/sc_admin/category" class="nav-link">
+							<a href="#" class="nav-link">
 								<i class="fas fa-folder-open nav-icon"></i>
 								<p>Categories</p>
 							</a>
