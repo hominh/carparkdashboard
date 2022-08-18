71bb1e546145c1844356fae91c4bedf94d4c88d1
Ho Minh <minhhh12@gmail.com>
2022-08-18 11:08:00 +0700

    Remove comment

--- core/base/resources/views/partials/nav.blade.php
+++ core/base/resources/views/partials/nav.blade.php
@@ -5,17 +5,17 @@
 		</li>
 		<li class="nav-item dropdown">
 			<a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false">
-				<img src="https://demo.s-cart.org/data/language/flag_uk.png" style="height: 25px;">
+				<img src="#" style="height: 25px;">
 			</a>
 			<div class="dropdown-menu dropdown-menu-left p-0" style="left: inherit; left: 0px;">
-				<a href="https://demo.s-cart.org/sc_admin/locale/en" class="dropdown-item  disabled">
+				<a href="#" class="dropdown-item  disabled">
 					<div class="hover">
-						<img src="https://demo.s-cart.org/data/language/flag_uk.png" style="height: 25px;"> English
+						<img src="#" style="height: 25px;"> English
 					</div>
 				</a>
-				<a href="https://demo.s-cart.org/sc_admin/locale/vi" class="dropdown-item ">
+				<a href="#" class="dropdown-item ">
 					<div class="hover">
-						<img src="https://demo.s-cart.org/data/language/flag_vn.png" style="height: 25px;"> Tiếng Việt
+						<img src="#" style="height: 25px;"> Tiếng Việt
 					</div>
 				</a>
 			</div>
@@ -23,27 +23,27 @@
 		<li class="nav-item dropdown">
 			<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">Theme</a>
 			<div class="dropdown-menu dropdown-menu-left p-0">
-				<a href="https://demo.s-cart.org/sc_admin/theme/lightblue" class="dropdown-item  disabled active">
+				<a href="#" class="dropdown-item  disabled active">
 					<div class="hover">
 						Lightblue
 					</div>
 				</a>
-				<a href="https://demo.s-cart.org/sc_admin/theme/dark" class="dropdown-item  ">
+				<a href="#" class="dropdown-item  ">
 					<div class="hover">
 						Dark
 					</div>
 				</a>
-				<a href="https://demo.s-cart.org/sc_admin/theme/blue" class="dropdown-item  ">
+				<a href="#" class="dropdown-item  ">
 					<div class="hover">
 						Blue
 					</div>
 				</a>
-				<a href="https://demo.s-cart.org/sc_admin/theme/white" class="dropdown-item  ">
+				<a href="#" class="dropdown-item  ">
 					<div class="hover">
 						White
 					</div>
 				</a>
-				<a href="https://demo.s-cart.org/sc_admin/theme/pink" class="dropdown-item  ">
+				<a href="#" class="dropdown-item  ">
 					<div class="hover">
 						Pink
 					</div>
@@ -51,7 +51,7 @@
 			</div>
 		</li>
 	</ul>
-	<form action="https://demo.s-cart.org/sc_admin/order" method="get" class="form-inline ml-3 d-none d-sm-block">
+	<form action="#" method="get" class="form-inline ml-3 d-none d-sm-block">
 		<div class="input-group input-group-sm">
 			<input name="keyword" class="form-control form-control-navbar" type="search" placeholder="Search order" aria-label="Search">
 			<div class="input-group-append">
@@ -62,7 +62,7 @@
 		</div>
 	</form>
 	<ul class="navbar-nav ml-auto">
-		<a class="nav-link" href="https://demo.s-cart.org" target=_new>
+		<a class="nav-link" href="#" target=_new>
 			<i class="fas fa-home"></i>
 		</a>
 		<li class="nav-item dropdown">
@@ -73,16 +73,16 @@
 			<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
 				<span class="dropdown-item dropdown-header">You have 11 new orders</span>
 				<div class="dropdown-divider"></div>
-				<a href="https://demo.s-cart.org/sc_admin/order?order_status=1" class="dropdown-item dropdown-footer">View more</a>
+				<a href="#" class="dropdown-item dropdown-footer">View more</a>
 			</div>
 		</li>
 		<li class="nav-item dropdown user-menu">
 			<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">
-				<img src="https://demo.s-cart.org/admin/avatar/user.jpg" class="user-image" alt="User Image">
+				<img src="#" class="user-image" alt="User Image">
 			</a>
 			<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
 				<div class="text-center">
-					<img src="https://demo.s-cart.org/admin/avatar/user.jpg" class="img-circle" alt="Test">
+					<img src="#" class="img-circle" alt="Test">
 					<div>
 						Test<br>
 						<small>Member since 2021-09-29 09:38:56</small>
@@ -90,10 +90,10 @@
 				</div>
 				<div class="user-footer">
 					<div class="float-left">
-						<a href="https://demo.s-cart.org/sc_admin/auth/setting" class="btn btn-default btn-flat">Setting</a>
+						<a href="#" class="btn btn-default btn-flat">Setting</a>
 					</div>
 					<div class="float-right">
-						<a href="https://demo.s-cart.org/sc_admin/auth/logout" class="btn btn-default btn-flat">Logout</a>
+						<a href="#" class="btn btn-default btn-flat">Logout</a>
 					</div>
 				</div>
 			</div>
