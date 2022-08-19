11f973e1b4896e12c9e240fe059756801e5ab1d4
Ho Minh <minhhh12@gmail.com>
2022-08-19 14:34:54 +0700

    Update info table

--- core/lot/resources/views/table/table-info.blade.php
+++ core/lot/resources/views/table/table-info.blade.php
@@ -1,6 +1,6 @@
 <span class="dt-length-records">
     <i class="fa fa-globe"></i>
     <span class="d-none d-sm-inline">Hiển thị</span>
-    _START_ đến _END_ in
+    _START_ đến _END_ trong
     <span class="badge badge-secondary bold badge-dt">_TOTAL_</span> bản ghi
 </span>
